cb1f1495e75430a73dd170d9868363857820a93a
pradeep-jais <pradeepjais13@gmail.com>
2023-04-23 15:07:09 +0530

    props in details, passing different props using different object, children props

--- src/tutorials/books-project-1/Booklist.js
+++ src/tutorials/books-project-1/Booklist.js
@@ -1,9 +1,17 @@
 import './booklist.css';
 
-const img = './images/book-1.jpg';
-const title = 'The Psychology of Money';
-const author = 'Morgan Housel';
-const titleStyle = { color: '#ff0000', backgroundColor: 'palegreen' };
+const firstBook = {
+  image: './images/book-1.jpg',
+  title: 'The Psychology of Money',
+  author: 'Morgan Housel',
+  titleStyle: { color: '#ff0000', backgroundColor: 'palegreen' },
+};
+const secondBook = {
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/81WCSu2RoJS._AC_UL900_SR900,600_.jpg',
+  title: 'Animals Tales From Panchtantra',
+  author: ' Wonder House Books',
+  titleStyle: { color: '#ffff00', backgroundColor: 'palegreen' },
+};
 
 const Booklist = () => {
   return (
@@ -11,15 +19,24 @@ const Booklist = () => {
       <h1 className="title">Best selling books at Amazon</h1>
       <section className="booklist">
         <Book
-          image={img}
-          name={title}
-          author={author}
+          image={firstBook.image}
+          name={firstBook.title}
+          author={firstBook.author}
           subject="social"
           number={20}
         />
-        <Book image={img} name={title} author={author} style={titleStyle} />
-        <Book image={img} name={title} author={author} />
-        <Book image={img} name={title} author={author} />
+        <Book
+          image={secondBook.img}
+          name={secondBook.title}
+          author={secondBook.author}
+          style={secondBook.titleStyle}
+        />
+        <Book
+          image={secondBook.img}
+          name={secondBook.title}
+          author={secondBook.author}
+          style={secondBook.titleStyle}
+        />
       </section>
     </main>
   );
@@ -27,11 +44,12 @@ const Booklist = () => {
 
 const Book = (props) => {
   // console.log(props);
+  const { image, name, style, author } = props;
   return (
     <article className="book">
-      <img src={props.image} alt={props.name} />
-      <h2 style={props.style}>{props.name}</h2>
-      <h4>{props.author.toUpperCase()} </h4>
+      <img src={image} alt={name} />
+      <h2 style={style}>{name}</h2>
+      <h4>{author.toUpperCase()} </h4>
     </article>
   );
 };
