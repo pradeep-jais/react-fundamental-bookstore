3cfc025e551887608c03332f2958f386efa81692
pradeep-jais <pradeepjais13@gmail.com>
2023-04-23 17:51:08 +0530

    booklist project - passing props by iteratiing on array using map, key prop,passing whole object as prop, object destructuring

--- src/tutorials/books-project-1/Booklist.js
+++ src/tutorials/books-project-1/Booklist.js
@@ -1,42 +1,30 @@
 import './booklist.css';
 
-const firstBook = {
-  image: './images/book-1.jpg',
-  title: 'The Psychology of Money',
-  author: 'Morgan Housel',
-  titleStyle: { color: '#ff0000', backgroundColor: 'palegreen' },
-};
-const secondBook = {
-  img: 'https://images-eu.ssl-images-amazon.com/images/I/81WCSu2RoJS._AC_UL900_SR900,600_.jpg',
-  title: 'Animals Tales From Panchtantra',
-  author: ' Wonder House Books',
-  titleStyle: { color: '#ffff00', backgroundColor: 'palegreen' },
-};
+const books = [
+  {
+    id: 1,
+    image: './images/book-1.jpg',
+    title: 'The Psychology of Money',
+    author: 'Morgan Housel',
+    titleStyle: { color: '#ff0000', backgroundColor: 'palegreen' },
+  },
+  {
+    id: 2,
+    image: 'https://m.media-amazon.com/images/I/81WCSu2RoJS.jpg',
+    title: 'Animals Tales From Panchtantra',
+    author: ' Wonder House Books',
+    titleStyle: { color: '#ffff00', backgroundColor: 'palegreen' },
+  },
+];
 
 const Booklist = () => {
   return (
     <main className="books-center">
       <h1 className="title">Best selling books at Amazon</h1>
       <section className="booklist">
-        <Book
-          image={firstBook.image}
-          name={firstBook.title}
-          author={firstBook.author}
-          subject="social"
-          number={20}
-        />
-        <Book
-          image={secondBook.img}
-          name={secondBook.title}
-          author={secondBook.author}
-          style={secondBook.titleStyle}
-        />
-        <Book
-          image={secondBook.img}
-          name={secondBook.title}
-          author={secondBook.author}
-          style={secondBook.titleStyle}
-        />
+        {books.map((book) => {
+          return <Book key={book.id} {...book} />;
+        })}
       </section>
     </main>
   );
