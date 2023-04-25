653733392b3795c50ac6a8dee6ed07133ba31869
pradeep-jais <pradeepjais13@gmail.com>
2023-04-25 16:46:08 +0530

    prop drilling, ES6 module - code refactor, images in src folder- image optimised

--- src/tutorials/books-project-1/Booklist.js
+++ src/tutorials/books-project-1/Booklist.js
@@ -1,73 +1,25 @@
 import './booklist.css';
-
-const books = [
-  {
-    id: 1,
-    image: './images/book-1.jpg',
-    title: 'The Psychology of Money',
-    author: 'Morgan Housel',
-    titleStyle: { color: '#ff0000', backgroundColor: 'palegreen' },
-  },
-  {
-    id: 2,
-    image: 'https://m.media-amazon.com/images/I/81WCSu2RoJS.jpg',
-    title: 'Animals Tales From Panchtantra',
-    author: ' Wonder House Books',
-    titleStyle: { color: '#ffff00', backgroundColor: 'palegreen' },
-  },
-];
+import { books } from './bookData';
+import Book from './Book';
 
 const Booklist = () => {
+  const getBook = (id) => {
+    const newBook = books.find((book) => book.id === id);
+    console.log(newBook);
+  };
+
   return (
     <main className="books-center">
       <h1 className="title">Best selling books at Amazon</h1>
-      <InputForm />
       <section className="booklist">
-        {books.map((book) => {
-          return <Book key={book.id} {...book} />;
+        {books.map((book, index) => {
+          return (
+            <Book key={book.id} {...book} getBook={getBook} number={index} />
+          );
         })}
       </section>
     </main>
   );
 };
-
-// Single book component
-const Book = (props) => {
-  // console.log(props);
-  const { image, name, style, author } = props;
-  return (
-    <article className="book">
-      <img src={image} alt={name} />
-      <h2 style={style}>{name}</h2>
-      <h4>{author.toUpperCase()} </h4>
-    </article>
-  );
-};
-
-// form component Event example
-function InputForm() {
-  const handleFormInput = () => {
-    console.log('Handle form input event');
-  };
-  const handleButtonClick = () => {
-    alert('Handle button click event');
-  };
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    console.log('YOur form is submitted successfuly!');
-  };
-  return (
-    <form onSubmit={handleFormSubmit}>
-      <h2>Typical form</h2>
-      <input
-        type="text"
-        name="message"
-        onChange={handleFormInput}
-        style={{ margin: '1rem' }}
-      />
-      <button onClick={handleButtonClick}>submit form</button>
-    </form>
-  );
-}
 
 export default Booklist;
---
+++ src/tutorials/books-project-1/bookData.js
@@ -0,0 +1,28 @@
+// import book images
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
+export const books = [
+  {
+    id: 1,
+    image: img1,
+    title: 'The Psychology of Money',
+    author: 'Morgan Housel',
+    titleStyle: { color: '#ff0000', backgroundColor: 'palegreen' },
+  },
+  {
+    id: 2,
+    image: img2,
+    title: 'Animals Tales From Panchtantra',
+    author: ' Wonder House Books',
+    titleStyle: { color: '#ffff00', backgroundColor: 'palegreen' },
+  },
+  {
+    id: 3,
+    image: img3,
+    title: 'The Hidden Hindu',
+    author: 'Akshat Gupta',
+    titleStyle: { color: '#ffff00', backgroundColor: 'palegreen' },
+  },
+];
